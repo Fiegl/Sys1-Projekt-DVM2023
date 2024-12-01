285d50febc4c0eddc0771a0cded60a8f8e264324
Fiegl <167548499+Fiegl@users.noreply.github.com>
2024-12-01 21:02:18 +0100

    Update urls.py

--- meine_app/urls.py
+++ meine_app/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('arbeitsbericht_erstellen/', views.arbeitsbericht_erstellen_view, name='arbeitsbericht_erstellen'),
     path('arbeitsbericht_speichern/', views.arbeitsbericht_speichern, name='arbeitsbericht_speichern'),
     path('arbeitsberichte_anzeigen/', views.arbeitsberichte_anzeigen_view, name='arbeitsberichte_anzeigen'),
-    
-]
+    path('download_drucken/', views.download_drucken_view, name='download_drucken'),
+    path("bericht/<int:bericht_id>/", views.arbeitsberichte_download_drucken_view, name="arbeitsberichte_download_drucken"),
+    path("bericht/<int:bericht_id>/json/", views.bericht_download_json_view, name="bericht_download_json"),
+    path("bericht/<int:bericht_id>/csv/", views.bericht_download_csv_view, name="bericht_download_csv"),
