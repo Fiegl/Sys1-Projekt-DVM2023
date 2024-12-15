d613ed93c3d694a87448cb894e9c8f18156c5ebc
Fiegl <167548499+Fiegl@users.noreply.github.com>
2024-12-15 17:16:11 +0100

    Update urls.py
    die aktualisierte urls.py

--- meine_app/urls.py
+++ meine_app/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from . import views
+from .views import BerichtDownloadView  # Import der Klassenbasierten View für Downloads
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('home/', views.home_view, name='home'),
     path('logout/', views.logout_view, name='logout'),
+
     path('arbeitsbericht_erstellen/', views.arbeitsbericht_erstellen_view, name='arbeitsbericht_erstellen'),
     path('arbeitsbericht_speichern/', views.arbeitsbericht_speichern, name='arbeitsbericht_speichern'),
+
     path('arbeitsberichte_anzeigen/', views.arbeitsberichte_anzeigen_view, name='arbeitsberichte_anzeigen'),
-    path('download_drucken/', views.download_drucken_view, name='download_drucken'),
-    path("bericht/<int:bericht_id>/", views.arbeitsberichte_download_drucken_view, name="arbeitsberichte_download_drucken"),
-    path("bericht/<int:bericht_id>/json/", views.bericht_download_json_view, name="bericht_download_json"),
-    path("bericht/<int:bericht_id>/csv/", views.bericht_download_csv_view, name="bericht_download_csv"),
+    path('loeschen/<int:bericht_id>/', views.loesche_bericht, name='bericht_loeschen'),
+
+    path('arbeitsberichte_download_drucken/', views.arbeitsberichte_download_drucken_view, name='arbeitsberichte_download_drucken'),
+    path('download/<str:format>/<int:bericht_id>/', views.BerichtDownloadView.as_view(), name='bericht_herunterladen'),
+    path('bericht_hochladen/', views.bericht_hochladen, name='bericht_hochladen'),
+    
 ]
