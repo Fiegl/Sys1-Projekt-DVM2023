270ffdb64b2c485c722045c1bbd7160d27e9289f
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-01-30 19:07:55 +0100

    Update urls.py

--- meine_app/urls.py
+++ meine_app/urls.py
@@ -11,11 +11,13 @@ urlpatterns = [
     path('arbeitsbericht_speichern/', views.arbeitsbericht_speichern, name='arbeitsbericht_speichern'),
 
     path('arbeitsberichte_anzeigen/', views.arbeitsberichte_anzeigen_view, name='arbeitsberichte_anzeigen'),
-    path('arbeitsberichte/loeschen/<str:bericht_id>/', views.arbeitsbericht_loeschen, name='arbeitsbericht_loeschen'),
+    path('arbeitsberichte_loeschen/<str:bericht_id>/', views.arbeitsbericht_loeschen, name='arbeitsbericht_loeschen'),
     
    
     path('arbeitsberichte_download_drucken/', views.arbeitsberichte_download_drucken_view, name='arbeitsberichte_download_drucken'),
-    path('bericht_herunterladen/<str:format>/<str:bericht_id>/', views.bericht_herunterladen, name='bericht_herunterladen'),
+    path('bericht_herunterladen/json/<str:bericht_id>/', views.bericht_als_json, name='bericht_herunterladen_json'),
+    path('bericht_herunterladen/csv/<str:bericht_id>/', views.bericht_als_csv, name='bericht_herunterladen_csv'),
+    path('bericht_herunterladen/xml/<str:bericht_id>/', views.bericht_als_xml, name='bericht_herunterladen_xml'),
     path('bericht_hochladen/', views.bericht_hochladen, name='bericht_hochladen'),
 
 
@@ -25,13 +27,11 @@ urlpatterns = [
     path('status_downgrade/', views.status_downgrade, name='status_downgrade'),
     path('benutzer_sperren/', views.benutzer_sperren, name='benutzer_sperren'),
     path('benutzer_entsperren/', views.benutzer_entsperren, name='benutzer_entsperren'),
-
+    
     path('module_edit/', views.module_edit, name='module_edit'),
     path('neues_modul_abspeichern/', views.neues_modul_abspeichern, name='neues_modul_abspeichern'), 
 
     path('profile_page_user/', views.profile_page_user_view, name='profile_page_user'),
-    path('status_upgrade_anfrage/', views.status_upgrade_anfrage, name='status_upgrade_anfrage'),    
-
-    path('beispiel/', views.beispiel_view, name='beispiel'),
+    path('status_upgrade_anfrage/', views.status_upgrade_anfrage, name='status_upgrade_anfrage'),
     
 ]
